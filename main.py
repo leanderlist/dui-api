50d2b6941fd0ebe4c579de1f4e56272297887ade
leanderlist <llist@student.tgm.ac.at>
2023-03-14 13:03:20 +0000

    Adding base functionalities to the program

--- main.py
+++ main.py
@@ -12,8 +12,23 @@ client = discord.Client(intents=intents)
 with open('./insyBot/config.json', 'r') as configreader:
     config = json.load(configreader)
 
+db = mariadb.connect(
+      host="db",
+      port=3306,
+      user="root",
+      password="mariadb-dui-api"
+)
+
+cursor = db.cursor()
+
+
+
 @client.event
 async def on_ready():
+    global cursor, db
     print(f'{client.user} is now online!')
+    
+    cursor.execute("CREATE DATABASE IF NOT EXISTS data;")
+    cursor.close()
 
 client.run(config['token'])
