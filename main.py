15816194a452ce4c3e5786da0ee0f02faac8c1d8
leanderlist <llist@student.tgm.ac.at>
2023-03-14 13:02:05 +0000

    Adding basic Discord bot functionality

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import json
 import discord
 import mariadb
 from discord.ext import tasks
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+with open('./insyBot/config.json', 'r') as configreader:
+    config = json.load(configreader)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is now online!')
+
+client.run(config['token'])
