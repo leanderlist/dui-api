30c6b93e18870dcc6a485f3404219d7140046471
leanderlist <llist@student.tgm.ac.at>
2023-03-18 15:51:17 +0000

    Adding more user-information to the database

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 import discord
 import mariadb
 from discord.ext import tasks
@@ -6,6 +7,7 @@ from discord.ext import tasks
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.presences = True
 
 client = discord.Client(intents=intents)
 
@@ -17,7 +19,7 @@ db = mariadb.connect(
       port=3306,
       user="root",
       password="mariadb-dui-api"
-    )
+)
 
 cursor = db.cursor()
 
@@ -37,7 +39,7 @@ async def on_ready():
       database="data"
     )
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS userinfo( id BIGINT UNSIGNED PRIMARY KEY, name VARCHAR(64), tag INTEGER, avatarurl VARCHAR(255));")
+    cursor.execute("CREATE TABLE IF NOT EXISTS userinfo(id BIGINT UNSIGNED PRIMARY KEY, name VARCHAR(64), tag INTEGER, status VARCHAR(15), avatar VARCHAR(255), banner VARCHAR(255), creationdate DATE);")
     cursor.close()
     userdata.start()
 
@@ -48,9 +50,11 @@ async def userdata():
     members = client.guilds[0].members
     for i in members:
       cursor = db.cursor()
-      if i.id != client.user.id:
-        cursor.execute(f"INSERT INTO userinfo (id, name, tag, avatarurl) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE id=?, name=?, tag=?, avatarurl=?",
-        (i.id,i.name,int(i.discriminator),str(i.avatar if i.avatar != None else i.default_avatar), i.id,i.name,int(i.discriminator),str(i.avatar if i.avatar != None else i.default_avatar)))
+      if not i.bot:
+        date = datetime.strptime(str(i.created_at)[:10], '%Y-%m-%d')
+        user = await client.fetch_user(i.id)
+        cursor.execute(f"INSERT INTO userinfo (id, name, tag, status, avatar, banner, creationdate) VALUES (?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE id=?,name=?,tag=?,status=?,avatar=?,banner=?,creationdate=?",
+        (i.id, i.name, int(i.discriminator), str(i.status), str(i.avatar if i.avatar != None else i.default_avatar), str(user.banner), date, i.id, i.name, int(i.discriminator), str(i.status), str(i.avatar if i.avatar != None else i.default_avatar), str(user.banner), date))
       cursor.close()
     db.commit()
 
