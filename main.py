54335d55deded0c6510119cabbf5e553d13dce20
leanderlist <llist@student.tgm.ac.at>
2023-04-16 19:46:24 +0100

    Adding watching functionality

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ db = mariadb.connect(
       host="db",
       port=3306,
       user="root",
-      password="mariadb-dui-api"
+      password="NathanIstCool"
 )
 
 cursor = db.cursor()
@@ -41,7 +41,8 @@ async def on_ready():
     cursor = db.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS userinfo(id BIGINT UNSIGNED PRIMARY KEY, name VARCHAR(64), tag INTEGER, status VARCHAR(15), avatar VARCHAR(255), banner VARCHAR(255), creationdate DATE);")
     cursor.execute("CREATE TABLE IF NOT EXISTS spotifyactivity(trackid VARCHAR(25), userid BIGINT UNSIGNED, songname VARCHAR(255), artists VARCHAR(255), albumcover VARCHAR(255), album VARCHAR(255), songurl VARCHAR(255), startedplaying BIGINT, PRIMARY KEY (trackid, userid), FOREIGN KEY (userid) REFERENCES userinfo(id));")
-    cursor.execute("CREATE TABLE IF NOT EXISTS playingactivity(gamename VARCHAR(255), userid BIGINT UNSIGNED, state VARCHAR(255), gamedetails VARCHAR(255), startedplaying BIGINT, large_image_url VARCHAR(255), small_image_url VARCHAR(255), PRIMARY KEY (gamename, userid), FOREIGN KEY (userid) REFERENCES userinfo(id));")
+    cursor.execute("CREATE TABLE IF NOT EXISTS playingactivity(name VARCHAR(255), userid BIGINT UNSIGNED, state VARCHAR(255), details VARCHAR(255), startedplaying BIGINT, large_image_url VARCHAR(255), small_image_url VARCHAR(255), PRIMARY KEY (name, userid), FOREIGN KEY (userid) REFERENCES userinfo(id));")
+    cursor.execute("CREATE TABLE IF NOT EXISTS watchingactivity(name VARCHAR(255), userid BIGINT UNSIGNED, state VARCHAR(255), details VARCHAR(255), startedwatching BIGINT, large_image_url VARCHAR(255), small_image_url VARCHAR(255), PRIMARY KEY (name, userid), FOREIGN KEY (userid) REFERENCES userinfo(id));")
     cursor.close()
     userdata.start()
 
@@ -70,9 +71,13 @@ async def userdata():
               (a.track_id, i.id, a.title, save_artists, a.album_cover_url, a.album, a.track_url, a.start, a.track_id, i.id, a.title, save_artists, a.album_cover_url, a.album, a.track_url, a.start))
 
           if a.type == discord.ActivityType.playing:
-            cursor.execute("INSERT INTO playingactivity(gamename, userid, state, gamedetails, startedplaying, large_image_url, small_image_url) VALUES (?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE gamename=?, userid=?, state=?, gamedetails=?, startedplaying=?, large_image_url=?, small_image_url=?",
+            cursor.execute("INSERT INTO playingactivity(name, userid, state, gamedetails, startedplaying, large_image_url, small_image_url) VALUES (?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE name=?, userid=?, state=?, details=?, startedplaying=?, large_image_url=?, small_image_url=?",
               (a.name, i.id, a.state, a.details, a.start, a.large_image_url, a.small_image_url, a.name, i.id, a.state, a.details, a.start, a.large_image_url, a.small_image_url))
-
+          
+          if a.type == discord.ActivityType.watching:
+            cursor.execute("INSERT INTO watchingactivity(name, userid, state, details, startedwatching, large_image_url, small_image_url) VALUES (?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE name=?, userid=?, state=?, details=?, startedwatching=?, large_image_url=?, small_image_url=?",
+              (a.name, i.id, a.state, a.details, a.start, a.large_image_url, a.small_image_url, a.name, i.id, a.state, a.details, a.start, a.large_image_url, a.small_image_url))
+      
       cursor.close()
     db.commit()
 
